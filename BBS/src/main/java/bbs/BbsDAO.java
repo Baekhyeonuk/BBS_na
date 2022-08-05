b0c42f0949dae2a1e5433db62a92d58b9949ccdc
Baekhyeonuk <zjfl3122@naver.com>
2022-08-05 13:36:02 +0900

    게시판 디자인, 글쓰기 기능 구현 bbs.jsp / Bbs.java / write.jsp / writeAction.jsp / BbsDAO.java 파일 추가
    게시판(메인) 페이지(bbs.jsp) 디자인, 게시판 DB 구축 + Bbs.java
    글쓰기 기능구현(write.jsp, BbsDAO.java, writeAction.jsp)

---
+++ BBS/src/main/java/bbs/BbsDAO.java
@@ -0,0 +1,72 @@
+package bbs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BbsDAO {  // 데이터 접근 객체의 약자 - DAO  데이터베이스 접근을 해서 데이터를 빼오는 역할을 하는 클래스
+	
+	private Connection conn;
+	private ResultSet rs;
+	
+	public BbsDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			String dbID = "root";
+			String dbPassword = "0000";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getDate() {
+		String SQL = "SELECT NOW()";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {  // 결과가 있는 경우
+				return rs.getString(1);  // 현재 날짜를 그대로 반환
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ""; // 데이터베이스 오류
+	}
+	
+	public int getNext() {
+		String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {  // 결과가 있는 경우
+				return rs.getInt(1) +1;  // 현재 날짜를 그대로 반환
+			}
+			return 1; // 첫번째 게시글인 경우
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 데이터베이스 오류
+	}
+	
+	public int write(String bbsTitle, String userID, String bbsContent) {
+		String SQL = "INSERT INTO BBS VALUES (?, ?, ?, ?, ?, ?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext());
+			pstmt.setString(2, bbsTitle);
+			pstmt.setString(3, userID);
+			pstmt.setString(4, getDate());
+			pstmt.setString(5, bbsContent);
+			pstmt.setInt(6, 1);
+			
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 데이터베이스 오류
+	}
+	
+}
